530e6360755fdac0051a25cf4b6a3e2eb13c75c7
Your Name <offirs12@gmail.com>
2018-07-25 19:05:26 +0300

    Ofir backend

--- routes/tripRoutes.js
+++ routes/tripRoutes.js
@@ -1,4 +1,43 @@
-function banana(){}
-module.exports = {
-    banana
+const TRIP_URL = '/trips';
+const tripService = require('../services/tripService')
+// const reviewService = require('../services/reviewService')
+
+
+function addTripRoutes(app) {
+    app.get('/trips', (req, res) => {
+        console.log('swalala2');
+        tripService.query()
+        .then((trips) => res.json(trips))
+        .then(trips => res.send(trips))
+    })
+
+    app.get('/trips/:tripId', (req, res) => {
+        const tripId = req.params.tripId;
+        tripService.getById(tripId)
+            .then(trip => res.json(trip))
+    })
+    
+    app.delete('/trips/:tripId', (req, res) => {
+        const tripId = req.params.tripId;
+        tripService.remove(tripId)
+            .then(() => res.end(`trip ${tripId} Deleted `))
+    })
+
+    app.post('/trips', (req, res) => {
+        const trip = req.body;
+        tripService.add(trip)
+            .then(trip => {
+                res.json(trip)
+            })
+    })
+
+    app.put('/trips/:tripId', (req, res) => {
+        const trip = req.body;
+        tripService.update(trip)
+            .then(trip => res.json(trip))
+    })
+
 }
+
+module.exports = addTripRoutes
+
